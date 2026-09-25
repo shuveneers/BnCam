d1b288d46fe716cfd492cd2cda776ef76e3586c2
Banananana <s.vreemdeling@gmail.com>
2026-09-25 09:11:46 +0200

    RAW zero-denoise: remove neural JNI API

--- app/src/main/java/com/bncam/core/vulkan/VulkanNativeBridge.kt
+++ app/src/main/java/com/bncam/core/vulkan/VulkanNativeBridge.kt
@@ -13,8 +13,6 @@ internal object VulkanNativeBridge {
         pipelineCachePath: String
     ): Int
 
-    external fun nativeConfigureSpectraNeuralModel(packageBytes: ByteArray): Boolean
-
     external fun nativeShutdown(): Int
     external fun nativeGetRuntimeStateCode(): Int
     external fun nativeGetSchemaVersion(): Int
